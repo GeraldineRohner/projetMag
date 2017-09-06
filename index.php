caef2c9ee25493da05c51c73e11cb7659bd0c71c
HaydeeL <haydee.leconte@gmail.com>
2017-09-06 15:28:32 +0200

    page acceuil mise a jour

--- index.php
+++ index.php
@@ -11,11 +11,53 @@
     </head>
     <body>
 
-<?php echo "bonjour"; ?>
+
+        <div class="container">
+
+          <!-- Static navbar -->
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Connexion</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="../navbar-static-top/">Admin</a></li>
+          </ul>
+      </div><!--/.nav-collapse -->
+  </div><!--/.container-fluid -->
+</nav>
+
+<div class="container-fluid">
+
+    <h1 style="text-align: center; padding: 20px; font-size: 70px;">WebZine</h1>
+
+</div>
+
+<!-- Main component for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <h2>Article</h2>
+    <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+    <p>
+        <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View more</a>
+    </p>
+</div>
+
+</div>
 
 
-    	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    	<script src="scripts/script.js"></script>
-    </body>
-    </html>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="scripts/script.js"></script>
+</body>
+</html>
